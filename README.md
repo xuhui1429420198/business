d28d57eef07801fa29e7c4dab9aabdab6570fad9
xuhui86 <xuhui18@jd.com>
2020-08-05 13:24:01 +0800

    feat: 经营情况页面

--- README.md
+++ README.md
@@ -7,8 +7,8 @@ deploy
 
 需要有node环境 & npm
 + 安装依赖 npm i
-+ node-sass可能会安装失败 这是再执行 npm install node-sass 
-+ 安装 npm install antd-mobile 组件库
-+ 安装 npm install babel-plugin-import
++ node-sass可能会安装失败 换个淘宝镜像 这是再执行 cnpm install sass-loader node-sass   [React配置scss]:https://blog.csdn.net/etemal_bright/article/details/85104690
++ 本地启动 npm run start 具体命令可以参考 package.json
+
 
 
--- src/containers/Pages/IndexPage.js
+++ src/containers/Pages/IndexPage.js
@@ -1,9 +1,10 @@
 /**
  * xuhui86 主页
+ * https://editor.csdn.net/md/?articleId=85104690
  */
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import { WingBlank, Tabs, WhiteSpace, List, InputItem, Picker, Toast, DatePicker, Checkbox, ImagePicker } from 'antd-mobile';
+import { WingBlank, Tabs, WhiteSpace, List, InputItem, Picker, Toast, DatePicker, Checkbox, ImagePicker, Modal, Button} from 'antd-mobile';
 
 import '../themes/pages/index.scss'
 const addressDemo = [
@@ -104,13 +105,37 @@ const licenseTypeList = [
     label: '个体工商户',
     value: '2',
   },
-]
+];
+const comanyNumList = [
+  {
+    label: '0-10',
+    value: '1',
+  },
+  {
+    label: '11-100',
+    value: '2',
+  },
+  {
+    label: '100以上',
+    value: '3',
+  },
+];
+function closest(el, selector) {
+  const matchesSelector = el.matches || el.webkitMatchesSelector || el.mozMatchesSelector || el.msMatchesSelector;
+  while (el) {
+    if (matchesSelector.call(el, selector)) {
+      return el;
+    }
+    el = el.parentElement;
+  }
+  return null;
+}
 
 class DataPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      LicenseImgList: [], // 营业执照照片
+      LicenseImgList: [], // 营业执照照片 图片渲染格式参考imgsFileList
       multiple: false, // 图片是否多选
       current: 0, // 当前选中的tab index
       societyCreditCode: '',  // 社会信用代码
@@ -131,6 +156,39 @@ class DataPage extends Component {
       weixin: '', // 微信
       contactAreaList: addressDemo,
       contactArea: ['02', '02-1', '02-1-1'],
+      comanyNumList, // 公司人数list
+      comanyNumType: ['1'],//公司人数list 选中的item
+      modal1: false,
+      mainClassText: '请选择',
+      mainClassList:[
+        {
+          presetsName:'孕妇',
+          presetsCode:'1054',
+          adeptCategoryList:[{
+            title:'孕妇装',
+            id: 197
+          }]
+        },
+        {
+          presetsName: '母婴',
+          presetsCode: '1055',
+          adeptCategoryList: [{
+            title: '女装',
+            id: 195
+          }, {
+              title: '童装',
+              id: 196
+            }]
+        },
+      ],
+      isExperience: true,
+      companyImgList: [], // 公司img 1张
+      officeImgList: [], //办公室img
+      wareHouseImgList: [], // 仓库img
+      carHouseImgList: [], // 车间img
+      hotProductImgList: [], // 热销产品img
+      otherImgList: [], // 其他图片 上限5张
+      buttonText: '下一步',
     }
   }
   componentWillMount() {
@@ -164,18 +222,17 @@ class DataPage extends Component {
     } = this.state;
     return (
       <div className='base-info-content'>
-        <form>
           <List>
             <List.Item>
               <div>营业执照</div>
-              <div>营业执照上需印有公章或手印，小于10M。请注意核对信息。</div>
+              <div className='gray-text'>营业执照上需印有公章或手印，小于10M。请注意核对信息。</div>
             </List.Item>
             <ImagePicker
               length="5"
               files={LicenseImgList}
-              onChange={this.onLicenseImgChange}
-              onImageClick={this.onLicenseImgClick}
-              selectable={LicenseImgList.length < 7}
+            onChange={this.onLicenseImgChange.bind(this, 'LicenseImgList')}
+            onImageClick={this.onLicenseImgClick.bind(this, 'LicenseImgList')}
+              selectable={LicenseImgList.length < 1}
             />
             <InputItem
               placeholder="请输入"
@@ -304,22 +361,19 @@ class DataPage extends Component {
               联系人手机
             </InputItem>
           </List>
-        </form>
 
       </div>);
   }
-  onLicenseImgChange = (files, type, index) => {
+  onLicenseImgChange = (name, files, type, index) => {
     console.log(files, type, index);
     this.setState({
-      LicenseImgList: files,
+      [name]: files,
     });
   };
-  onLicenseImgClick = (index, fs) => {
-    console.log(index, fs)
+  onLicenseImgClick = (name,index, fs) => {
+    console.log(name,index, fs)
   }
-  onTabChange = (key) => {
-    console.log(key);
-  };
+
   onErrorClick = (name, value) => {
     switch (name) {
       case 'societyCreditCode':
@@ -352,32 +406,228 @@ class DataPage extends Component {
   }
 
 
-
+  
   // 渲染经营情况
   renderSituation() {
     const {
-      
+      comanyNumList,
+      comanyNumType,
+      isExperience,
+      companyImgList,
+      officeImgList,
+      wareHouseImgList,
+      carHouseImgList,
+      hotProductImgList,
+      otherImgList,
+      mainClassText,
     } = this.state;
     return (
       <div className='situation-content'>
         <List>
           <Picker
-            data={licenseTypeList}
+            data={comanyNumList}
             title="公司人数"
             cols={1}
-            value={licenseType}
-            onOk={e => this.setState({ licenseType: e })}
+            value={comanyNumType}
+            onOk={e => this.setState({ comanyNumType: e })}
             className="forss">
-            <List.Item arrow="horizontal">营业执照类型</List.Item>
+            <List.Item arrow="horizontal">公司人数</List.Item>
           </Picker>
+          <List.Item 
+            arrow="horizontal"
+            extra={mainClassText}
+            onClick={() => this.setState({modal1: true})}
+          >
+            主营品类
+          </List.Item>
+          
+        </List>
+        <WhiteSpace/>
+        <List>
+          <List.Item>
+            电商平台经验
+            <Checkbox.CheckboxItem
+              checked={isExperience}
+              onChange={e => this.setState({ isExperience: e.target.checked })} >
+              有
+            </Checkbox.CheckboxItem>
+            <Checkbox.CheckboxItem
+              checked={!isExperience}
+              onChange={e => this.setState({ isExperience: !e.target.checked })} >
+              无
+            </Checkbox.CheckboxItem>
+          </List.Item>
+        </List>
+        <WhiteSpace />
+        <List>
+          <List.Item>
+            <div>
+              经营图片
+              <img src="https://statics.sheinside.cn/resource/nj-fed-dist/srm-mobile/7c0067c/dist/fd945699b7253e6f7e179275a644c8db.png"></img>
+            </div>
+            <div>营业执照上需印有公章或手印，小于10M。请注意核对信息。</div>
+          </List.Item>
+          <div className='mansgeImgsList'>
+            <div className='img-box'>
+              <div className='top-content'>
+                <ImagePicker
+                  length="1"
+                  files={companyImgList}
+                  onChange={this.onLicenseImgChange.bind(this, 'companyImgList')}
+                  onImageClick={this.onLicenseImgClick.bind(this, 'companyImgList')}
+                  selectable={companyImgList.length < 1}
+                />
+              </div>
+              <div className='bottom-content'>
+                公司全景
+              </div> 
+            </div>
+            <div className='img-box'>
+              <div className='top-content'>
+                <ImagePicker
+                  length="1"
+                  files={officeImgList}
+                  onChange={this.onLicenseImgChange.bind(this, 'officeImgList')}
+                  onImageClick={this.onLicenseImgClick.bind(this, 'officeImgList')}
+                  selectable={officeImgList.length < 1}
+                />
+              </div>
+              <div className='bottom-content'>
+                办公室
+              </div>
+            </div>
+            <div className='img-box'>
+              <div className='top-content'>
+                <ImagePicker
+                  length="1"
+                  files={wareHouseImgList}
+                  onChange={this.onLicenseImgChange.bind(this, 'wareHouseImgList')}
+                  onImageClick={this.onLicenseImgClick.bind(this, 'wareHouseImgList')}
+                  selectable={wareHouseImgList.length < 1}
+                />
+              </div>
+              <div className='bottom-content'>
+                仓库
+              </div>
+            </div>
+            <div className='img-box'>
+              <div className='top-content'>
+                <ImagePicker
+                  length="1"
+                  files={carHouseImgList}
+                  onChange={this.onLicenseImgChange.bind(this, 'carHouseImgList')}
+                  onImageClick={this.onLicenseImgClick.bind(this, 'carHouseImgList')}
+                  selectable={carHouseImgList.length < 1}
+                />
+              </div>
+              <div className='bottom-content'>
+                车间
+              </div>
+            </div>
+            <div className='img-box'>
+              <div className='top-content'>
+                <ImagePicker
+                  length="1"
+                  files={hotProductImgList}
+                  onChange={this.onLicenseImgChange.bind(this, 'hotProductImgList')}
+                  onImageClick={this.onLicenseImgClick.bind(this, 'hotProductImgList')}
+                  selectable={hotProductImgList.length < 1}
+                />
+              </div>
+              <div className='bottom-content'>
+                热销产品
+              </div>
+            </div>
+          </div>
+          <div className='otherImgList'>
+            <ImagePicker
+              length="5"
+              files={otherImgList}
+              onChange={this.onLicenseImgChange.bind(this, 'otherImgList')}
+              onImageClick={this.onLicenseImgClick.bind(this, 'otherImgList')}
+              selectable={otherImgList.length < 5}
+            />
+            <div className='bottom-content'>其他</div>
+          </div>
+          
         </List>
       </div>
     )
   }
-  onTabClick = (tab, index) => {
-    console.log('onTabClick', index, tab);
+
+  renderModal(){
+    const { 
+      modal1,
+      mainClassList,
+     } = this.state
+    return(
+      <div>
+        {modal1}
+        <Modal
+          popup
+          animationType="slide-up"
+          visible={modal1}
+          onClose={this.onClose('modal1')}
+        >
+          <div className='list-content'>
+            <div className='left'>
+              <List>
+                {mainClassList.map((mainClass, index) => (
+                  <List.Item key={index}>{mainClass.presetsName}</List.Item>
+                ))}
+              </List>
+            </div>
+            <div className='right'>
+              <List>
+                {mainClassList.map((mainClass) => (
+                  mainClass.adeptCategoryList.map((item, index) => (
+                    <List.Item key={index}>{item.title}</List.Item>
+                  ))
+                ))}
+              </List>
+            </div>
+            
+          </div>
+        </Modal>
+      </div>
+     
+    )
+  }
+
+  onClose = key => () => {
     this.setState({
-      current: index
+      [key]: false,
+    });
+  }
+
+  renderButton(){
+    const { buttonText } = this.state;
+    return(
+      <div className='button-content'>
+        <div className='button' onClick={this.buttonClick}>{buttonText}</div>
+      </div>
+    )
+  }
+
+  buttonClick = () =>{
+    const { buttonText } = this.state;
+    if (buttonText === '下一步') {
+      this.setState({
+        buttonText: '提交',
+        current: 1,
+      })
+    } 
+  }
+  onTabClick = (tab, index) => {
+    let buttonText = '';
+    if (index == 0) {
+      buttonText = '下一步'
+    } else {
+      buttonText = '提交'
+    }
+    this.setState({
+      current: index,
+      buttonText
     })
   }
 
@@ -386,11 +636,13 @@ class DataPage extends Component {
       { title: '基本信息' },
       { title: '经营情况' },
     ];
+    const { current} = this.state;
     return (
       <div className="IndexPage">
 
         <Tabs tabs={tabs}
           initialPage={0}
+          page={current}
           onTabClick={this.onTabClick}
         >
           <div className='item-box'>
@@ -405,7 +657,8 @@ class DataPage extends Component {
             </WingBlank>
           </div>
         </Tabs>
-
+        {this.renderButton()}
+        {this.renderModal()}
       </div>
     );
   }
